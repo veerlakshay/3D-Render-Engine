8a9634de77c448d50c4a841539e346418aaa7caf
Lakshay <lakshay.rv@gmail.com>
2024-11-10 23:10:33 -0500

    3D shape tranformation

--- src/threeDRenderEngine/DemoViewer.java
+++ src/threeDRenderEngine/DemoViewer.java
@@ -60,16 +60,37 @@ public class DemoViewer {
 
                 //rendering will happen here
 
-                g2.translate(getWidth() / 2 , getHeight() / 2);
+                double heading = Math.toRadians(headingSlider.getValue());
+
+                Matrix3 headingTransform = new Matrix3(new double[] {
+                        Math.cos(heading), 0, Math.sin(heading),
+                        0, 1, 0,
+                        -Math.sin(heading), 0, Math.cos(heading)
+                });
+                double pitch = Math.toRadians(pitchSlider.getValue());
+                Matrix3 pitchTransform = new Matrix3(new double[] {
+                        1, 0, 0,
+                        0, Math.cos(pitch), Math.sin(pitch),
+                        0, -Math.sin(pitch), Math.cos(pitch)
+                });
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
+
+                g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.WHITE);
                 for (Triangle t : tris) {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
                     Path2D path = new Path2D.Double();
-                    path.moveTo(t.v1.x, t.v1.y);
-                    path.lineTo(t.v2.x, t.v2.y);
-                    path.lineTo(t.v3.x, t.v3.y);
+                    path.moveTo(v1.x, v1.y);
+                    path.lineTo(v2.x, v2.y);
+                    path.lineTo(v3.x, v3.y);
                     path.closePath();
                     g2.draw(path);
                 }
+
+                headingSlider.addChangeListener(e -> this.repaint());
+                pitchSlider.addChangeListener(e -> this.repaint());
             }
         };
 
